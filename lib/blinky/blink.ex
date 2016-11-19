475137f76d211365150c6cf9289456bd5a63d7c0
Roy Veshovda <roy@veshovda.no>
2016-11-19 13:24:21 +0200

    Everything actually

---
+++ lib/blinky/blink.ex
@@ -0,0 +1,21 @@
+defmodule Blinky.Blink do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def init([]) do
+    {:ok, pid} = Gpio.start_link(14, :output)
+    spawn(fn -> blink_forever(pid) end)
+    {:ok, []}
+  end
+
+  defp blink_forever(pid) do
+    Gpio.write(pid, 1)
+    :timer.sleep(1000)
+    Gpio.write(pid, 0)
+    :timer.sleep(1000)
+    blink_forever(pid)
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,43 @@
+defmodule Blinky.Mixfile do
+  use Mix.Project
+
+  @target System.get_env("NERVES_TARGET") || "rpi3"
+
+  def project do
+    [app: :blinky,
+     version: "0.0.1",
+     target: @target,
+     archives: [nerves_bootstrap: "~> 0.1.4"],
+     deps_path: "deps/#{@target}",
+     build_path: "_build/#{@target}",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     aliases: aliases,
+     deps: deps ++ system(@target)]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [mod: {Blinky, []},
+     applications: [:logger, :elixir_ale]]
+  end
+
+  def deps do
+    [
+      {:nerves, "~> 0.3.0"},
+      {:elixir_ale, "~> 0.5.6"}
+    ]
+  end
+
+  def system(target) do
+    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  end
+
+  def aliases do
+    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
+     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
+  end
+
+end
